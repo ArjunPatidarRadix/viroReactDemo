4e0fcdcbaec9882ca19c50635c9310542215c1b1
Arjun <arjun@grublr.com>
2024-02-29 19:22:08 +0530

    ThreejsDemo work (29-2-2024)

--- ThreeJSDemo/metro.config.js
+++ ThreeJSDemo/metro.config.js
@@ -6,6 +6,14 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+const defaultConfig = getDefaultConfig(__dirname);
+
+const config = {
+  resolver: {
+    sourceExts: ['js', 'jsx', 'json', 'ts', 'tsx', 'cjs'],
+    assetExts: ['glb', 'gltf', 'mtl', 'obj', 'png', 'jpg'],
+  },
+};
+
+module.exports = mergeConfig(defaultConfig, config);
---
+++ ThreeJSDemo/src/Nike3d.tsx
@@ -0,0 +1,98 @@
+import {Canvas, useFrame, useLoader} from '@react-three/fiber';
+import {Asset} from 'expo-asset';
+import {Suspense, useLayoutEffect, useMemo, useRef, useState} from 'react';
+import {Text} from 'react-native';
+import THREE from 'three';
+import {OBJLoader} from 'three/examples/jsm/loaders/OBJLoader';
+import {MTLLoader} from 'three/examples/jsm/loaders/MTLLoader';
+import {TextureLoader} from 'three';
+// import {useAnimatedSensor, SensorType} from 'react-native-reanimated';
+
+export const Nike3d = () => {
+  const Box = props => {
+    const meshRef = useRef();
+    const [active, setActive] = useState(false);
+
+    useFrame((state, delta) => {
+      if (active) {
+        meshRef.current.rotation.x += delta;
+        meshRef.current.rotation.y += delta;
+        meshRef.current.rotation.z += delta;
+      }
+    });
+    return (
+      <mesh
+        ref={meshRef}
+        {...props}
+        scale={active ? 1.5 : 0.5}
+        onClick={event => setActive(!active)}>
+        <torusKnotGeometry />
+        <meshStandardMaterial color={active ? 'green' : 'gray'} />
+      </mesh>
+    );
+  };
+
+  //   const Shoe = props => {
+  //     const meshRef = useRef();
+  //     const [base, normal, rough] = useLoader(TextureLoader, [
+  //       require('../res/Airmax/textures/BaseColor.jpg'),
+  //       require('../res/Airmax/textures/Normal.jpg'),
+  //       require('../res/Airmax/textures/Roughness.png'),
+  //     ]);
+  //     const material = useLoader(MTLLoader, require('../res/Airmax/shoe.mtl'));
+  //     const buffer = useLoader(
+  //       THREE.FileLoader,
+  //       require('../res/Airmax/shoe.obj'),
+  //       loader => {
+  //         material.preload();
+  //         loader.setMaterials(material);
+  //       },
+  //     );
+  //     useLayoutEffect(() => {
+  //       obj.traverse(child => {
+  //         if (child instanceof THREE.Mesh) {
+  //           child.material.map = base;
+  //           child.material.normalMap = normal;
+  //           child.material.roughnessMap = rough;
+  //         }
+  //       });
+  //     }, []);
+  //     const obj = useMemo(
+  //       () => new OBJLoader().parse(THREE.LoaderUtils.decodeText(buffer)),
+  //       [buffer],
+  //     );
+  //     useFrame((state, delta) => {
+  //       let {x, y, z} = props.animatedSensor.sensor.value;
+  //       x = ~~(x * 100) / 5000;
+  //       y = ~~(y * 100) / 5000;
+  //       meshRef.current.rotation.x += x;
+  //       meshRef.current.rotation.y += y;
+  //     });
+  //     return (
+  //       <mesh rotation={[0.7, 0, 0]} ref={meshRef}>
+  //         <primitive object={obj} scale={10} />
+  //       </mesh>
+  //     );
+  //   };
+  //   const animatedSensor = useAnimatedSensor(SensorType.GYROSCOPE, {
+  //     interval: 100,
+  //   });
+
+  return (
+    <Canvas>
+      <ambientLight />
+      <pointLight
+        position={[10, 10, 10]}
+        intensity={1000}
+        color="#fff"
+        distance={100}
+      />
+      <Box />
+      <Box position={[0, 2, 0]} />
+      <Box position={[0, -2, 0]} />
+      {/* <Suspense fallback={null}>
+        <Shoe animatedSensor={animatedSensor} />
+      </Suspense> */}
+    </Canvas>
+  );
+};
