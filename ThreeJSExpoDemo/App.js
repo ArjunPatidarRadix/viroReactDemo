a778d78a94d1ef4e055bd0b7d1831a0087d597d7
Arjun <arjun@grublr.com>
2024-03-01 18:56:34 +0530

    updated

--- ThreeJSExpoDemo/App.js
+++ ThreeJSExpoDemo/App.js
@@ -1,25 +1,5 @@
-import { SafeAreaView, StyleSheet, View } from "react-native";
 import { Shoe3D } from "./src/Shoe3D";
 
 export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <Shoe3D />
-    </SafeAreaView>
-  );
+  return <Shoe3D />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#f6f7fb",
-  },
-  modalContainer: {
-    height: "40%",
-    overflow: "visible",
-  },
-  detailContainer: {
-    flex: 1,
-    backgroundColor: "white",
-  },
-});
